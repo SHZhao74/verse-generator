19a56ba0c6c0a86a52855cc39abf5e619f390b70
Shou Hao Zhao <j5201314peter@gmail.com>
2019-08-11 11:25:17 +0800

    no message

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const logger = require("morgan");
 const path = require("path");
+const favicon = require("serve-favicon");
 
 const Util = require("./src/utli");
 const LyricRouter = require("./routes/LyricRouter");
@@ -15,7 +16,7 @@ const port = process.env.PORT || 3000;
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use( favicon(path.join(__dirname, "public", "img", "favicon.ico")) );
 if (app.get("env") === "production") {
   console.log("==========PRODUCTION Mode==========");
   app.set("trust proxy", 1); // trust first proxy
